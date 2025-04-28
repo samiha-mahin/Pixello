330b001f8f63f4b290563f6a86264828928ab3d7
samiha-mahin <samihamahin36@gmail.com>
2025-04-28 22:48:29 +0600

    s

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
 
+dotenv.config();
 const app = express();
 
 //middlewares
@@ -15,8 +18,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 8000;
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+ app.listen (PORT,()=>{
+    connectDB();
+    console.log(`Server Running at Port ${PORT}`);
+ });
--- server/utils/db.js
+++ server/utils/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try {
-        await mongoose.connect(process.env.MONGO_URI),
-        console.log("MongoDB connected successfully")
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully");
     } catch (error) {
         console.log(error.message);
     }
 }
+export default connectDB;
