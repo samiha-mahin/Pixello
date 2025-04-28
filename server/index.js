bc2662ff8db7a683477ec14bd1be1c990e459e97
samiha-mahin <samihamahin36@gmail.com>
2025-04-28 22:07:07 +0600

    s

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-//middlewares
 
-app.use(express.json());
-app.use(cookieParser());
-app.use
+//middlewares
+app.use(express.json()); //Parses JSON payloads from request body.
+app.use(express.urlencoded({extended:true})); //Parses URL-encoded payloads (e.g., forms).
+app.use(cookieParser()); //Parses cookies from incoming HTTP requests
+
+const corsOptions = {
+    origin: ' http://localhost:5173/',
+  credentials: true, // Ensures cookies are sent
+};
+app.use(cors(corsOptions));
 
 const PORT = 8000;
 
