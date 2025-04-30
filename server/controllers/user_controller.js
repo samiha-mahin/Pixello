c53f24630c51a932cc7badffb65c76ce7463e519
samiha-mahin <samihamahin36@gmail.com>
2025-04-30 13:22:11 +0600

    s

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -6,38 +6,114 @@ import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post_model.js";
 
 export const register = async (req, res) => {
-    try {
-        const {username, email, password} = req.body;
-        if(!username || !email || !password) {
-            return res.status(400).json({
-                message: "All fields are required",
-                success: false,
-            });
-        }
-
-        const user = await User.findOne({email});
-        if (user) {
-            return res.status(400).json({
-              message: "User already exists with this email",
-              success: false,
-            });
-          }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await User.create({
-            username,
-            email,
-            password: hashedPassword,
-        });
-        return res.status(201).json({
-            message: "User created successfully",
-            success: true,
-        });
-      
-    } catch (error) {
-        return res.status(500).json({
-            message: "Internal server error",
-            success: false,
-        });
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
     }
-}
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists with this email",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      message: "User created successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    const populatedPosts = await Promise.all(
+      user.posts.map(async (postId) => {
+        const post = await Post.findOne({ _id: postId });
+        if (post.author.equals(user._id)) {
+          return post;
+        }
+        return null;
+      })
+    );
+    // user.posts contains IDs of the user's posts
+    // Using .map() to go through each post ID
+    // Fetch the full post document by ID
+    // Double-check if the post's author is the same user
+    // If valid, include the post; otherwise return null
+    // Promise.all ensures all async operations complete before moving on
+
+    user = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        bio: user.bio,
+        followers: user.followers,
+        following: user.following,
+        posts: populatedPosts
+    }
+    return res
+    .status(200)
+    .cookie("token", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpsOnly: true,
+      sameSite: "strict",
+    })
+    .json({
+      message: `Welcome back ${user.username}`,
+      user,
+      success: true,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
