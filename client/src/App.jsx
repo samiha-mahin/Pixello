d134fd694c9f58d71b6c6443e80e27825ab80fc0
samiha-mahin <samihamahin36@gmail.com>
2025-05-05 00:04:06 +0600

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from './components/Signup'
+import Login from './components/Login'
+import MainLayout from './components/MainLayout'
+import Home from './components/Home'
+import Profile from './components/Profile'
 
 const App = () => {
+  // const { user } = useSelector(store => store.auth);
+  // const { socket } = useSelector(store => store.socketio);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   if (user) {
+  //     const socketio = io('http://localhost:8000', {
+  //       query: { userId: user?._id },
+  //       transports: ['websocket']
+  //     });
+
+  //     dispatch(setSocket(socketio));
+
+  //     socketio.on('getOnlineUsers', (onlineUsers) => {
+  //       dispatch(setOnlineUsers(onlineUsers));
+  //     });
+
+  //     socketio.on('notification', (notification) => {
+  //       dispatch(setLikeNotification(notification));
+  //     });
+
+  //     return () => {
+  //       socketio.close();
+  //       dispatch(setSocket(null));
+  //     };
+  //   } else if (socket) {
+  //     socket.close();
+  //     dispatch(setSocket(null));
+  //   }
+  // }, [user, dispatch]);
   return (
    <>
    <BrowserRouter>
    <Routes>
     <Route path="/signup" element={<Signup />} /> 
+    <Route path="/login" element={<Login />} />
+     {/* Protected routes inside MainLayout */}
+     <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />}/>
+          <Route path="profile/:id" element={<Profile />} />
+      </Route>
    </Routes>
    </BrowserRouter>
    </>
---
+++ client/src/utils/constant.js
@@ -0,0 +1,3 @@
+export const USER_API = "http://localhost:8000/api/v1/user";
+export const Post_API = "http://localhost:8000/api/v1/post";
+export const MSG_API = "http://localhost:8000/api/v1/message";
