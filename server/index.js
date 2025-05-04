eff1504e16dccf1c15623e5d076d44c87d48f6c1
samiha-mahin <samihamahin36@gmail.com>
2025-05-05 02:09:52 +0600

    s

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true})); //Parses URL-encoded payloads (e.g
 app.use(cookieParser()); //Parses cookies from incoming HTTP requests
 
 const corsOptions = {
-    origin: ' http://localhost:5173/',
+    origin: ' http://localhost:5173',
   credentials: true, // Ensures cookies are sent
 };
 app.use(cors(corsOptions));
