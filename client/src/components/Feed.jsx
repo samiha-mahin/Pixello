117c6d9a7625f30308100e995af542b0f1bd4f0e
samiha-mahin <samihamahin36@gmail.com>
2025-05-05 23:41:52 +0600

    social-media

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Posts from './Posts'
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='flex-1 my-8 flex flex-col items-center pl-[20%]'>
+        <Posts/>
+    </div>
   )
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Feed from './Feed'
+import { Outlet } from 'react-router-dom'
+import RightSidebar from './RightSidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div>
+            <Feed/>
+            <Outlet/>
+        </div>
+        <RightSidebar/>
+    </div>
   )
 }
 
