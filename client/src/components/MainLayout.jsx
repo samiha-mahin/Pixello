07754882260a2175f8044d2954e9e141a3757ca9
samiha-mahin <samihamahin36@gmail.com>
2025-05-05 12:32:12 +0600

    social-media

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import LeftSidebar from './LeftSidebar'
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+         <LeftSidebar/>
+        <div>
+             <Outlet/>  
+            {/* Outlet is where the nested route's component appears inside a layout like MainLayout.  */}
+        </div>
+    </div>
   )
 }
 
