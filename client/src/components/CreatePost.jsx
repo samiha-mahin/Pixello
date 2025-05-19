afe9bb8c0539add057e4f5ae6a755db00eaab397
samiha-mahin <samihamahin36@gmail.com>
2025-05-20 01:19:16 +0600

    s

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Dialog, DialogContent, DialogHeader } from './ui/dialog'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { useSelector } from 'react-redux';
 
-const CreatePost = () => {
+
+const CreatePost = ({open, setOpen}) => {
+  const {user} = useSelector(store => store.auth);
   return (
-    <div>CreatePost</div>
+    <Dialog open={open}>
+      <DialogContent  onInteractOutside={()=> setOpen(false)}>
+        <DialogHeader className="text-centre font-semibold" >Create Post</DialogHeader>
+        <div className='flex items-center gap-3'>
+          <Avatar>
+            <AvatarImage src={user?.profilePicture} alt=''/>
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div>
+            <h1 className='font-semibold text-xs'>{user?.username}</h1>
+            {/* <span className='text-gray-600 text-xs'>Bio here...</span> */}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 
