bfa82fb6cd1953ba81304675cc1f44e9880a10e1
samiha-mahin <samihamahin36@gmail.com>
2025-06-12 13:03:33 +0600

    s

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,13 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Dialog, DialogContent, DialogHeader } from './ui/dialog'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Textarea } from './ui/textarea';
 
 
 const CreatePost = ({open, setOpen}) => {
-  const imageRef = useRef();
+  const imageRef = useRef(); 
+  //useRef() is a React Hook that lets you store a reference to a DOM element (like an <img>, <input>, or any HTML tag).
+  const [file, setFile] = useState("");
+  const [caption, setCaption] = useState("");
+  const [imagePreview, setImagePreview] = useState("");
+  const [loading, setLoading] = useState(false);
   const {user} = useSelector(store => store.auth);
+  const {posts} = useSelector(store => store.post);
+  const dispatch = useDispatch();
+
+
+  
   return (
     <Dialog open={open}>
       <DialogContent  onInteractOutside={()=> setOpen(false)}>
